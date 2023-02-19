2644feb3525957acee0103f111164adecc020fb8
nift4 <nift4@protonmail.com>
2023-02-19 10:40:51 +0100

    BesLoudness: load default value from resources
    * and remove unused dependency
    
    Change-Id: Ibe3f7c80b13466b44a005570d4f2bb2e4328ce8f

--- BesLoudness/src/org/lineageos/settings/device/BesLoudnessManager.java
+++ BesLoudness/src/org/lineageos/settings/device/BesLoudnessManager.java
@@ -23,25 +23,30 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.util.Log;
 
+import org.lineageos.settings.besloudness.R;
+
 public class BesLoudnessManager {
     public static final String TAG = "BesLoudness";
     public static final String SHARED_PREFS_NAME = "besloudness";
     public static final String KEY_BESLOUDNESS = "besloudness";
-    public static final boolean DEFAULT_VALUE = true; // Matching with MTK Audio HAL
 
     public static void set(final Context context, Boolean value) {
+        final boolean defaultValue = context.getResources().getBoolean(
+            R.bool.besloudness_default_value);
         final SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFS_NAME, 0);
         if (value != null) {
             prefs.edit().putBoolean(KEY_BESLOUDNESS, value).apply();
         }
-        boolean newValue = prefs.getBoolean(KEY_BESLOUDNESS, DEFAULT_VALUE);
+        boolean newValue = prefs.getBoolean(KEY_BESLOUDNESS, defaultValue);
         final AudioManager amgr = (AudioManager) context.getSystemService("audio");
         amgr.setParameters("SetBesLoudnessStatus=" + (newValue ? "1" : "0"));
     }
 
     public static boolean get(final Context context) {
+        final boolean defaultValue = context.getResources().getBoolean(
+            R.bool.besloudness_default_value);
         final SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFS_NAME, 0);
-        boolean expectedValue = prefs.getBoolean(KEY_BESLOUDNESS, DEFAULT_VALUE);
+        boolean expectedValue = prefs.getBoolean(KEY_BESLOUDNESS, defaultValue);
         final AudioManager amgr = (AudioManager) context.getSystemService("audio");
         boolean actualValue = !("GetBesLoudnessStatus=0".equals(
                 amgr.getParameters("GetBesLoudnessStatus")));
