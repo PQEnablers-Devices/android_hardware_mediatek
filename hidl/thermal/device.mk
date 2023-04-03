094452b9cb5fe928c3d777241ec1fae7db8a2e41
Woomymy <woomy@woomy.be>
2023-04-03 10:45:39 +0200

    hidl: thermal: Reband to mediatek
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I568a8398d1a79167e36f53acc6805a93a09c4ccd

--- hidl/thermal/device.mk
+++ hidl/thermal/device.mk
@@ -1,14 +1,14 @@
 # Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.pixel
+    android.hardware.thermal@2.0-service.mediatek
 
 # Thermal utils
 PRODUCT_PACKAGES += \
-    thermal_symlinks
+    thermal_symlinks_mediatek
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
-    thermal_logd
+    thermal_logd_mediatek
 endif
 
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/thermal
