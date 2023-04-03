66f41bce6d99c4f9f763bb3496f0b668f8654260
Woomymy <woomy@woomy.be>
2023-04-03 10:47:04 +0200

    hidl: thermal: Don't install thermal_logd_mediatek on userdebug builds
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I32f80aa1d9dcfcf27d8e74b564537feb91336fd0

--- hidl/thermal/device.mk
+++ hidl/thermal/device.mk
@@ -6,7 +6,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     thermal_symlinks_mediatek
 
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
     thermal_logd_mediatek
 endif
